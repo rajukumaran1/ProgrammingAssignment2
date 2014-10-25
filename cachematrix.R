f8cd70c96443d866a46273a838ffcb0a9b24b970
rajukumaran1 <rajukumaran@gmail.com>
2014-10-25 17:21:01 -0400

    Commit the initial file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions in this file will save the matrix and its inverse. 
+## CacheSolve function has to be called in order to avoid the recomputing inverse multiple times.
 
-## Write a short comment describing this function
+## This function creates a list, which returns functions 
+## to access the matrix and inverse of it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  if(class(x)!="matrix")
+  {
+    print ("Invalid matrix")
+    return(NULL)
+  }
+  
+  m <- NULL
+  set <- function(y = matrix()) {
+    if(class(y)=="matrix"){
+      x <<- y
+      m <<- NULL
+    }
+    else
+    {
+      print("Input is not matrix")
+    }
+  }
+  
+  get <- function() x
+  
+  ##Set the Inverse
+  setInverse <- function(Inverse = matrix())
+  {
+    ## Checks if the inverse is a matrix. So, any other types will be ignored
+    if(class(Inverse)!="matrix")
+    {
+      print("Input is not matrix")
+    }
+    else
+    {
+      m <<- Inverse
+    }
+  }
+  
+  ##return the inverse stored in the method.
+  getInverse <- function() m
+  
+  ##The list containg the functions to access the matrix and its inverse.
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function will return the inverse of the matrix stored in the vector,
+## created by makeCacheMatrix function. If the inverse is not already stored in the vectory,
+## then this function will compute Inverse, store it n the vector and return the inverse. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
